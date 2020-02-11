fa448231ce8a78d0aa4f69e786ddfcc3bb420649
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-02-11 10:33:49 +0000

    yay completegit add .git add .! wooooooo

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,58 +1,43 @@
 public class Sudoku {
 
-    //AHHH 2 Dimensional ARRAYS OMG :(
-    public int[][] sudoku = {
-            {0,0,0,0,0,2,1,0,0},
-            {0,0,4,0,0,8,7,0,0},
-            {0,2,0,3,0,0,9,0,0},
-            {6,0,2,0,0,3,0,4,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,5,0,6,0,0,3,0,1},
-            {0,0,3,0,0,5,0,8,0},
-            {0,0,8,2,0,0,5,0,0},
-            {0,0,9,7,0,0,0,0,0},
-    };
+//    public static boolean solver (int[][] sudokuGrid) {
+//        for (int row = 0; row < sudokuGrid.length; row++) {
+//            for(int column = 0; column < sudokuGrid.length; column++) {
+//                if (sudokuGrid[row][column] == 0) {
+//                    for (int tryNumber = 1; tryNumber < 9; tryNumber++) {
+//                        sudokuGrid[row][column] = tryNumber;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
 
-    public static boolean solver (int[][] sudokuGrid) {
-        for (int row = 0; row < sudokuGrid.length; row++) {
-            for(int column = 0; column < sudokuGrid.length; column++) {
-                if (sudokuGrid[row][column] == 0) {
-                    for (int tryNumber = 1; tryNumber < 9; tryNumber++) {
-                        sudokuGrid[row][column] = tryNumber;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean isRowValid(int row, int tryNumber) {
-        for (int i = 0; i < sudoku.length; i++) {
-            if (sudoku[row][i] == tryNumber) {
+    public static boolean isRowValid(int[][] board, int row, int tryNumber) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == tryNumber) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean isColumnValid(int column, int tryNumber) {
-        for (int i = 0; i < sudoku.length; i++) {
-            if (sudoku[i][column] == tryNumber)
+    public static boolean isColumnValid(int[][] board, int column, int tryNumber) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] == tryNumber)
                 return true;
         }
         return false;
     }
 
-    public boolean is3x3Valid(int row, int column, int tryNumber) {
-        //getting the 3x3 grid
-//        int rowBox = row - (row % 3);
-//        int columnBox = column - (column % 3);
+    public static boolean is3x3Valid(int[][] board, int row, int column, int tryNumber) {
+//      getting the 3x3 grid
+        int rowBox = row - (row % 3);
+        int columnBox = column - (column % 3);
 
-        for (int x = 0; x < sudoku.length; x++) {
-            for (int y = 0; y < sudoku.length; x++) {
-                x = row  - (row % 3);
-                y = column - (column % 3);
-                if (sudoku[x][y] == tryNumber) {
+        for (int x = rowBox; x < rowBox + 3; x++) {
+            for (int y = columnBox; y < columnBox + 3; y++) {
+                if (board[x][y] == tryNumber) {
                     return true;
                 }
             }
@@ -60,10 +45,57 @@ public class Sudoku {
         return false;
     }
 
-    public boolean areNumbersValid(int row, int column, int tryNumber) {
-        if (!isRowValid(row, tryNumber) && !isColumnValid(column, tryNumber) && !is3x3Valid(row, column, tryNumber)) {
+    public static boolean areNumbersValid(int[][] board, int row, int column, int tryNumber) {
+        if (!isRowValid(board, row, tryNumber) && !isColumnValid(board, column, tryNumber) && !is3x3Valid(board, row, column, tryNumber)) {
             return true;
         }
         return false;
+    }
+
+    public static boolean solver(int[][] board) {
+        int row = 0;
+        int column = 0;
+        boolean isSpaceEmpty = true;
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                if (board[x][y] == 0) {
+                    //filling up the square with a number so it is no longer empty
+                    row = x;
+                    column = y;
+                    isSpaceEmpty = false;
+                    break;
+                }
+            }
+            //if there are still empty spaces
+            if (!isSpaceEmpty) {
+                break;
+            }
+        }
+        //if there is no empty spaces left
+        if (isSpaceEmpty) {
+            return true;
+        }
+
+        //backtracking stuff
+        for (int number = 1; number <= 9; number++) {
+            if (areNumbersValid(board, row, column, number)) {
+                board[row][column] = number;
+                if (solver(board)) {
+                    return true;
+                } else {
+                    //if number is invalid change it back to a 0
+                    board[row][column] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                System.out.println(board[x][y]);
+            }
+        }
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,30 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        //AHHH 2 Dimensional ARRAYS OMG :(
+        int[][] board = new int[][]{
+                {0, 0, 0, 0, 0, 2, 1, 0, 0},
+                {0, 0, 4, 0, 0, 8, 7, 0, 0},
+                {0, 2, 0, 3, 0, 0, 9, 0, 0},
+                {6, 0, 2, 0, 0, 3, 0, 4, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 5, 0, 6, 0, 0, 3, 0, 1},
+                {0, 0, 3, 0, 0, 5, 0, 8, 0},
+                {0, 0, 8, 2, 0, 0, 5, 0, 0},
+                {0, 0, 9, 7, 0, 0, 0, 0, 0},
+        };
 
+        if (Sudoku.solver(board)) {
+            String solved = Arrays
+                    .stream(board)
+                    .map(Arrays::toString)
+                    .collect(Collectors.joining(System.lineSeparator()));
+            System.out.println(solved);
+        } else  {
+            System.out.println("Unable to find solution");
+        }
     }
-
 }
